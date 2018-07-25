4686f79fb1fba22c92cab8c468203d8e9dd280ba
Chillup <philipkaim@gmail.com>
2018-07-25 10:35:24 -0400

    routing and views added

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 var faker = require('faker');
 var mysql = require('mysql');
- 
+var path = require('path');
+var bodyParser = require('body-parser');
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '/public')));
+
 var connection = mysql.createConnection({
   host       : 'localhost',
   user       : 'root',
@@ -8,11 +17,15 @@ var connection = mysql.createConnection({
   database   : 'node'
 });
 
-if (connection) {
-    console.log('WORKING');
-} else {
-    console.log('BROKE!!!!');
-}
+connection.on('error', function(err) {
+    console.log("[mysql error]",err);
+  });
+
+// if (connection) {
+//     console.log('WORKING');
+// } else {
+//     console.log('BROKE!!!!');
+// }
 
 
 // SELECTING ALL USERS
@@ -38,19 +51,46 @@ if (connection) {
 
 
 // INSERT NEW USERS IN BULK!!!
-var data = [];
+// var data = [];
 
-for (var i = 0; i < 500; i++) {
-    data.push(
-        [faker.internet.email(), faker.date.past()]
-    );
-}
+// for (var i = 0; i < 500; i++) {
+//     data.push(
+//         [faker.internet.email(), faker.date.past()]
+//     );
+// }
 
-var q = 'INSERT INTO users(email, created_at) VALUES ?';
+// var q = 'INSERT INTO users(email, created_at) VALUES ?';
 
-connection.query(q, [data], function (error, results, fields) {
-    if (error) throw error;
-    console.log(results);
- });
+// connection.query(q, [data], function (error, results, fields) {
+//     if (error) throw error;
+//     console.log(results);
+//  });
 
- connection.end();
+//  connection.end();
+
+app.get('/', function(req, res) {
+
+    var q = 'SELECT COUNT(*) AS count FROM users';
+
+    connection.query(q, function (error, results) {
+        if (error) throw error;
+        var count = results[0].count;
+        // res.send(`I have ${count} users in my database!`);
+        res.render('home', { data: count });
+    });
+});
+
+app.post('/register', function(req, res) {
+    var person = { email: req.body.email };
+
+    var q = 'INSERT INTO users SET ?';
+
+    connection.query(q, person, function (error, result) {
+        if (error) throw error;
+        res.redirect('/');
+     });
+});
+
+app.listen(8080, function() {
+    console.log('app running on port 8080!!!');
+});
